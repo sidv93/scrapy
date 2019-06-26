6265336c2f34021392ded65ee2a1ccc3997d6e3d
Siddharth <siddhuv93@gmail.com>
2019-06-26 22:58:32 +0530

    code done

--- start.js
+++ start.js
@@ -1,38 +1,55 @@
 import { getPriceFromBestBuy, getPriceFromAmazon, getPriceFromAmazonIndia } from './lib/index';
 import { models } from './models';
+import { startJob } from './lib/cron';
+import { sendMail } from './lib/mailer';
 
-( async () => {
-    const macbookProTouchBarBestbuy = await getPriceFromBestBuy(models.macbookProWithTouchBar.bestbuy);
-    console.log(`Price of Macbook pro with touch bar from BestBuy is ${macbookProTouchBarBestbuy}`);
+startJob();
+// ( async () => {
+//     const macbookProTouchBarBestbuy = await getPriceFromBestBuy(models.macbookProWithTouchBar.bestbuy);
+//     console.log(`Price of Macbook pro with touch bar from BestBuy is ${macbookProTouchBarBestbuy}`);
 
-    const macbookPro128Bestbuy = await getPriceFromBestBuy(models.macbookPro128.bestbuy);
-    console.log(`Price of Macbook pro 128GB from BestBuy is ${macbookPro128Bestbuy}`);
+//     const macbookPro128Bestbuy = await getPriceFromBestBuy(models.macbookPro128.bestbuy);
+//     console.log(`Price of Macbook pro 128GB from BestBuy is ${macbookPro128Bestbuy}`);
 
-    const macbookPro256Bestbuy = await getPriceFromBestBuy(models.macbookPro256.bestbuy);
-    console.log(`Price of Macbook pro 256GB from BestBuy is ${macbookPro256Bestbuy}`);
+//     const macbookPro256Bestbuy = await getPriceFromBestBuy(models.macbookPro256.bestbuy);
+//     console.log(`Price of Macbook pro 256GB from BestBuy is ${macbookPro256Bestbuy}`);
 
-    const macbookAir128Bestbuy = await getPriceFromBestBuy(models.macbookAir128.bestbuy);
-    console.log(`Price of Macbook Air 128GB from BestBuy is ${macbookAir128Bestbuy}`);
+//     const macbookAir128Bestbuy = await getPriceFromBestBuy(models.macbookAir128.bestbuy);
+//     console.log(`Price of Macbook Air 128GB from BestBuy is ${macbookAir128Bestbuy}`);
 
-    const macbookAir256Bestbuy = await getPriceFromBestBuy(models.macbookAir256.bestbuy);
-    console.log(`Price of Macbook Air 256GB from BestBuy is ${macbookAir256Bestbuy}`);
+//     const macbookAir256Bestbuy = await getPriceFromBestBuy(models.macbookAir256.bestbuy);
+//     console.log(`Price of Macbook Air 256GB from BestBuy is ${macbookAir256Bestbuy}`);
 
-    const macbookProTouchBarAmazon = await getPriceFromAmazon(models.macbookProWithTouchBar.amazon);
-    console.log(`Price of Macbook pro with touch bar from Amazon is ${macbookProTouchBarAmazon}`);
+//     const macbookProTouchBarAmazon = await getPriceFromAmazon(models.macbookProWithTouchBar.amazon);
+//     console.log(`Price of Macbook pro with touch bar from Amazon is ${macbookProTouchBarAmazon}`);
 
-    const macbookPro128Amazon = await getPriceFromAmazon(models.macbookPro128.amazon);
-    console.log(`Price of Macbook pro 128GB from Amazon is ${macbookPro128Amazon}`);
+//     const macbookPro128Amazon = await getPriceFromAmazon(models.macbookPro128.amazon);
+//     console.log(`Price of Macbook pro 128GB from Amazon is ${macbookPro128Amazon}`);
 
-    const macbookPro256Amazon = await getPriceFromAmazon(models.macbookPro256.amazon);
-    console.log(`Price of Macbook pro 256GB from Amazon is ${macbookPro256Amazon}`);
+//     const macbookPro256Amazon = await getPriceFromAmazon(models.macbookPro256.amazon);
+//     console.log(`Price of Macbook pro 256GB from Amazon is ${macbookPro256Amazon}`);
 
-    const macbookAir128Amazon = await getPriceFromAmazon(models.macbookAir128.amazon);
-    console.log(`Price of Macbook Air 128GB from Amazon is ${macbookAir128Amazon}`);
+//     const macbookAir128Amazon = await getPriceFromAmazon(models.macbookAir128.amazon);
+//     console.log(`Price of Macbook Air 128GB from Amazon is ${macbookAir128Amazon}`);
 
-    const macbookAir256Amazon = await getPriceFromAmazon(models.macbookAir256.amazon);
-    console.log(`Price of Macbook Air 256GB from Amazon is ${macbookAir256Amazon}`);
+//     const macbookAir256Amazon = await getPriceFromAmazon(models.macbookAir256.amazon);
+//     console.log(`Price of Macbook Air 256GB from Amazon is ${macbookAir256Amazon}`);
 
-    const sonyXm3Amazon = await getPriceFromAmazon(models.sonyXm3.amazon);
-    console.log(`Price of Sony wh-1000xm3 from Amazon is ${sonyXm3Amazon}`);
-})();
+//     const sonyXm3Amazon = await getPriceFromAmazon(models.sonyXm3.amazon);
+//     console.log(`Price of Sony wh-1000xm3 from Amazon is ${sonyXm3Amazon}`);
+
+//     sendMail({
+//         macbookProTouchBarBestbuy,
+//         macbookPro128Bestbuy,
+//         macbookPro256Bestbuy,
+//         macbookAir128Bestbuy,
+//         macbookAir256Bestbuy,
+//         macbookProTouchBarAmazon,
+//         macbookPro128Amazon,
+//         macbookPro256Amazon,
+//         macbookAir128Amazon,
+//         macbookAir256Amazon,
+//         sonyXm3Amazon
+//     });
+// })();
 
